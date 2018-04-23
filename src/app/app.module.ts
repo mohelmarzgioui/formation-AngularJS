f613de2854b8bc2e0d55f0fb34c65a4ed03ac0be
el marzgioui <mohamed.elmarzgioui@gmail.com>
2018-04-23 16:36:44 +0200

    formation angularjs4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    SharedModule,
+    CoreModule,
+    NgbModule.forRoot(),
+    HomeModule,
+    PageNotFoundModule
+  ],
+  providers: [],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
